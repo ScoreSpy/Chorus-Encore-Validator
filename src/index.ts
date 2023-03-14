8f31261c03d376f8aa056ea7228a556fb930757e
Ahriana <ahrianadev@gmail.com>
2023-03-14 07:47:46 +0000

    standalone support

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable guard-for-in */
 import './shim'
-import { fileExists, findSongs, replacePathPart, sanitizeFileName } from './helpers'
+import { askQuestion, fileExists, findSongs, keyPress, replacePathPart, sanitizeFileName } from './helpers'
 import { join, parse } from 'node:path'
 import { mkdir, writeFile } from 'node:fs/promises'
 import type { ApplicationArguments, SongData } from './types'
@@ -18,26 +18,46 @@ async function init () {
   let appArguments: ApplicationArguments
 
   if (isPackaged) {
-    const { argv } = yargs(process.argv.slice(2)).
-      option('baseDir', {
-        type: 'string',
-        description: 'The path to the base directory.',
-        demandOption: true
-      }).
-      option('outputDir', {
-        type: 'string',
-        description: 'The path to the output directory.',
-        demandOption: true
-      }).
-      option('dryRun', {
-        type: 'boolean',
-        description: 'Perform a dry run without actually copying files.',
-        default: false
-      }).
-      help().
-      alias('help', 'h')
+    if (process.argv.length > 2) {
+      const { argv } = yargs(process.argv.slice(2)).
+        option('baseDir', {
+          type: 'string',
+          description: 'The path to the base directory.',
+          demandOption: true
+        }).
+        option('outputDir', {
+          type: 'string',
+          description: 'The path to the output directory.',
+          demandOption: true
+        }).
+        option('dryRun', {
+          type: 'boolean',
+          description: 'Perform a dry run without actually copying files.',
+          default: false
+        }).
+        help().
+        alias('help', 'h')
 
-    appArguments = argv as ApplicationArguments
+      appArguments = argv as ApplicationArguments
+    } else {
+      setInterval(() => {
+        // nasty hack that stops the terminal quitting out
+      }, 2147483647)
+
+      appArguments = {
+        baseDir: process.cwd(),
+        outputDir: join(process.cwd(), 'CE'),
+        dryRun: false
+      }
+
+      console.log('\n')
+      console.log('baseDir: ', appArguments.baseDir)
+      console.log('outputDir: ', appArguments.outputDir)
+      console.log('dryRun: ', appArguments.dryRun)
+      console.log('\n')
+
+      await askQuestion('Infomation correct? "Yes" or "No"', 'yes')
+    }
   } else {
     const config = await import('./../devconfig.json')
 
@@ -72,10 +92,17 @@ async function init () {
     await logger.log('Metadata not writen to disk, Application ran in dry mode')
     await console.log('Metadata not writen to disk, Application ran in dry mode')
   } else {
-    await writeFile('output.json', JSON.stringify(output, null, 4))
+    await writeFile(join(appArguments.outputDir, 'output.json'), JSON.stringify(output, null, 4))
     await logger.log('Metadata writen to disk, upload this to your ScoreSpy dashboard to make them downloadable via Chorus Encore')
+    await logger.log(join(appArguments.outputDir, 'output.json'))
     await console.log('Metadata writen to disk, upload this to your ScoreSpy dashboard to make them downloadable via Chorus Encore')
+    await console.log(join(appArguments.outputDir, 'output.json'))
   }
 }
 
-init()
+init().then(() => {
+  keyPress()
+}).catch((e) => {
+  console.error((e as Error).message)
+  keyPress()
+})
